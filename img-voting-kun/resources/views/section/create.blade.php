a81ffb9b16f825466c65c6ae2c28371b876f3885
DESKTOP-CE2A35J\house <yimeistory@gmail.com>
2021-06-12 01:57:56 +0900

    [add]投票ページをいろいろ作ってみたのでテスト

--- img-voting-kun/resources/views/section/create.blade.php
+++ img-voting-kun/resources/views/section/create.blade.php
@@ -2,9 +2,28 @@
 
 @section("main")
     <form action="{{ route("section.store") }}">
+        @csrf
+        <input type="hidden" name="_method" value="post" />
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">写真投票</h5>
+                <h5 class="card-title">写真投稿</h5>
+                <h6 class="card-subtitle">投票対象になるポケモンスナップの画像をアップロードしてください。</h6>
+                <div class="form-group">
+                    <label for="name">投稿者名</label>
+                    <input type="name" class="form-control" id="name" aria-describedby="nameHelp">
+                    <small id="nameHelp" class="form-text text-muted">あなたの名前を入力してください。</small>
+                </div>
+                <fieldset>
+                    <div class="form-group">
+                        <label for="photo1">画像１</label>
+                        <input type="file" class="form-control-file" id="photo1" name="photo1">
+                    </div>
+                    <div class="form-group">
+                        <label for="photo1_detail">画像１の説明文</label>
+                        <input type="text" class="form-control" id="photo1_detail" aria-describedby="photo1DetailHelp">
+                        <small id="photo1DetailHelp" class="form-text text-muted">画像１の説明文を入力してください。そのままタイトルとして投票時に表示されます。</small>
+                    </div>
+                </fieldset>
             </div>
         </div>
     </form>
--- img-voting-kun/routes/web.php
+++ img-voting-kun/routes/web.php
@@ -17,9 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/test", function() {
-    return "test";
-});
-
 Route::get("/section/create", [\App\Http\Controllers\SectionController::class, "create"])->name("section.create");
 Route::post("/section/store", [\App\Http\Controllers\SectionController::class, "store"])->name("section.store");
